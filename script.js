2ccb81089808b02408e47ed58951055f85e5b54c
Ilkin999 <ilkin.qafarov.999@gmail.com>
2022-04-26 10:04:35 +0400

    exc

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ let rightCurrency = document.querySelector(".right-currency");
 let leftActiveButton = "RUB";
 let rightActiveButton = "USD";
 
+let activeInput;
+
 leftButtons.forEach(element => {
     element.addEventListener("click" , () => {
+        
         leftButtons.forEach(btn => {
             btn.classList.remove('active');
         })
@@ -24,7 +27,12 @@ leftButtons.forEach(element => {
                 rightActiveButton = rbtn.innerText; 
             }
         })
-        rightInputExchange();
+        if (activeInput == "left"){
+            leftInputExchange();
+        }
+        else{
+            rightInputExchange();
+        }
     })
     leftInputExchange();
 })
@@ -44,7 +52,12 @@ rightButtons.forEach(element => {
                 leftActiveButton = lbtn.innerText; 
             }
         })
-        leftInputExchange();
+        if (activeInput == "left"){
+            leftInputExchange();
+        }
+        else{
+            rightInputExchange();
+        }
     })
     rightInputExchange();
 })
@@ -52,33 +65,35 @@ rightButtons.forEach(element => {
 leftInput.addEventListener("input", leftInputExchange);
 
 function leftInputExchange () {
+    activeInput = "left";
     fetch(`https://api.exchangerate.host/latest?base=${leftActiveButton}&symbols=${rightActiveButton}`)
         .then(response => response.json())
         .then(data => {
             if (leftInput.value !== ""){
-                rightInput.value = Number(leftInput.value) * data.rates[`${rightActiveButton}`].toFixed(4);
+                rightInput.value = (Number(leftInput.value) * data.rates[`${rightActiveButton}`]).toFixed(3);
             }
             else{
                 rightInput.value = "";
             }
-            leftCurrency.innerText = `1 ${leftActiveButton} = ${data.rates[`${rightActiveButton}`].toFixed(4)} ${rightActiveButton}`
-            rightCurrency.innerText = `1 ${rightActiveButton} = ${(1 / data.rates[`${rightActiveButton}`]).toFixed(4)} ${leftActiveButton}`
+            leftCurrency.innerText = `1 ${leftActiveButton} = ${(data.rates[`${rightActiveButton}`]).toFixed(3)} ${rightActiveButton}`
+            rightCurrency.innerText = `1 ${rightActiveButton} = ${(1 / data.rates[`${rightActiveButton}`]).toFixed(3)} ${leftActiveButton}`
         })
 }
 
 rightInput.addEventListener("input", rightInputExchange);
 
 function rightInputExchange () {
+    activeInput = "right"
     fetch(`https://api.exchangerate.host/latest?base=${rightActiveButton}&symbols=${leftActiveButton}`)
         .then(response => response.json())
         .then(data => {
             if (rightInput.value !== ""){
-                leftInput.value = Number(rightInput.value) * data.rates[`${leftActiveButton}`].toFixed(4);
+                leftInput.value = (Number(rightInput.value) * data.rates[`${leftActiveButton}`]).toFixed(3);
             }
             else{
                 leftInput.value = "";
             }
-            rightCurrency.innerText = `1 ${rightActiveButton} = ${data.rates[`${leftActiveButton}`].toFixed(4)} ${leftActiveButton}`
-            leftCurrency.innerText = `1 ${leftActiveButton} = ${(1 / data.rates[`${leftActiveButton}`]).toFixed(4)} ${rightActiveButton}`
+            rightCurrency.innerText = `1 ${rightActiveButton} = ${(data.rates[`${leftActiveButton}`]).toFixed(3)} ${leftActiveButton}`
+            leftCurrency.innerText = `1 ${leftActiveButton} = ${(1 / data.rates[`${leftActiveButton}`]).toFixed(3)} ${rightActiveButton}`
         })
-} 
+}
